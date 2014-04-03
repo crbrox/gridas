38e1a7896e78c4229159a180e36cd5c6673ed09b
brox <brox@tid.es>
2014-04-03 08:24:55 +0200

    Update files with right version

--- src/gridas/bench/bench.go
+++ src/gridas/bench/bench.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/httptest"
+	"sync"
+	"time"
 )
 
 const benchURL = "http://localhost:8080/pirri"
@@ -12,38 +14,53 @@ const benchURL = "http://localhost:8080/pirri"
 var OK = []byte{'O', 'K'}
 var client = http.Client{}
 
+const N = 100
+const M = 200
+
+var wg sync.WaitGroup
+
 func main() {
-	BenchmarkE2ERoundGet()
+	wg.Add(N)
+	for i := 0; i < N; i++ {
+		go BenchmarkE2ERoundGet()
+	}
+	wg.Wait()
 }
 func BenchmarkE2ERoundGet() {
-	log.Println("begin")
+	defer wg.Done()
+	defer func() {
+		fmt.Println("*")
+	}()
 	gotCh := make(chan bool, 1000)
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		gotCh <- true
 		w.Write(OK)
-		log.Println("peticion recibida")
 	}))
 	defer ts.Close()
-	log.Println("Preparando request")
-	var request, err = http.NewRequest("GET", ts.URL, nil)
-	if err != nil {
-		panic(err)
+	for j := 0; j < M; j++ {
+		start := time.Now()
+		var request, err = http.NewRequest("GET", benchURL, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		request.Header.Set("x-relayer-host", ts.URL[len("http://"):])
+		resp, err := client.Do(request)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		if resp.StatusCode != 200 && resp.StatusCode != 201 {
+			fmt.Println("response status code != 200")
+			return
+		}
+		defer resp.Body.Close()
+		_, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		<-gotCh
+		fmt.Println(time.Since(start).Nanoseconds())
 	}
-	request.Header.Set("x-relayer-host", "churimiri")
-	log.Println("A punto de realizar request", request)
-	resp, err := client.Do(request)
-	if err != nil {
-		panic(err)
-	}
-	if resp.StatusCode != 200 {
-		panic("response status code != 200")
-	}
-	defer resp.Body.Close()
-	_, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	<-gotCh
-	//b.StopTimer()
 
 }
