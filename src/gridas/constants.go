293c77fe6f96bd53a0296c1c8d699027d2dd6245
brox <brox@tid.es>
2014-04-04 17:45:38 +0200

    Add missing constants file

---
+++ src/gridas/constants.go
@@ -0,0 +1,8 @@
+package gridas
+
+//Constants names of the header fields used by Rush
+const (
+	RelayerHost     = "X-Relayer-Host"
+	RelayerProtocol = "X-Relayer-Protocol"
+	RelayerTraceID  = "X-Relayer-Traceid"
+)
