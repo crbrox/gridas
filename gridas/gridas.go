2cd55170e18fbe2f39c09bc7ff94076abe3169c7
crbrox <carlos.romerobrox@telefonica.com>
2015-04-21 18:11:18 +0200

    Refactor to sue godep

---
+++ gridas/gridas.go
@@ -0,0 +1,91 @@
+// Executable of grush
+//
+// grush.ini contains configuration data read when starting at run-time
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/crbrox/gridas"
+	"github.com/crbrox/gridas/config"
+	"github.com/crbrox/gridas/mylog"
+
+	"labix.org/v2/mgo"
+)
+
+func main() {
+	var cfgFile = flag.String("config", "./gridas.yaml", "configuration file")
+	flag.Parse()
+	cfg, err := config.ReadConfig(*cfgFile)
+	if err != nil {
+		mylog.Alert(err)
+		os.Exit(-1)
+	}
+	fmt.Printf("configuration %q %+v\n", *cfgFile, cfg)
+
+	mylog.SetLevel(cfg.LogLevel)
+	mylog.Alert("hello World!")
+	reqChan := make(chan *gridas.Petition, cfg.QueueSize)
+	session, err := mgo.Dial(cfg.Mongo)
+	if err != nil {
+		mylog.Alert(err)
+		panic(err)
+	}
+	defer func() {
+		session.Close()
+		mylog.Debugf("mongo session closed %+v", session)
+	}()
+
+	mylog.Debugf("mongo session %+v", session)
+
+	db := session.DB(cfg.Database)
+	mylog.Debug("mongo database", db)
+	listener := &gridas.Listener{SendTo: reqChan, Cfg: cfg, SessionSeed: session}
+	mylog.Debugf("listener %+v", listener)
+	consumer := &gridas.Consumer{GetFrom: reqChan, Cfg: cfg, SessionSeed: session}
+	mylog.Debugf("consumer %+v", consumer)
+	rplyr := &gridas.Replyer{Cfg: cfg, SessionSeed: session}
+	mylog.Debugf("replyer %+v", rplyr)
+	rcvr := &gridas.Recoverer{SendTo: reqChan, Cfg: cfg, SessionSeed: session}
+	mylog.Debugf("recoverer %+v", rcvr)
+	endConsumers := consumer.Start(cfg.Consumers)
+	if err := rcvr.Recover(); err != nil {
+		mylog.Alert(err)
+		os.Exit(-1)
+	}
+	http.Handle("/", listener)
+	http.Handle("/responses/", http.StripPrefix("/responses/", rplyr))
+	go func() {
+		mylog.Debug("starting HTTP server (listener)")
+		err := http.ListenAndServe(":"+cfg.Port, nil)
+		if err != nil {
+			mylog.Alert(err)
+			os.Exit(-1)
+		}
+	}()
+
+	onEnd(func() {
+		mylog.Info("shutting down gridas ...")
+		listener.Stop()
+		mylog.Debug("listener stopped")
+		consumer.Stop()
+		mylog.Debug("consumer stopped")
+	})
+	<-endConsumers
+	mylog.Alert("bye World!")
+}
+
+func onEnd(f func()) {
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		s := <-sigCh
+		mylog.Debugf("Signal %+v received", s)
+		f()
+	}()
+}
---
+++ reply.go
@@ -0,0 +1,56 @@
+package gridas
+
+import (
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"labix.org/v2/mgo/bson"
+
+	"github.com/crbrox/gridas/mylog"
+)
+
+//Reply represents the response from the target host
+type Reply struct {
+	//Reply id. Currently the same as the petition id
+	ID      string `json:"id"`
+	TraceID string `json:"traceid"`
+	//Possible error in making the request. Could be ""
+	Error      string      `json:"error"`
+	StatusCode int         `json:"statuscode"` // e.g. 200
+	Proto      string      `json:"proto"`      // e.g. "HTTP/1.0"
+	Header     http.Header `json:"header"`
+	Trailer    http.Header `json:"trailer"`
+	Body       []byte      `json:"body"`
+	//Petition that
+	Petition *Petition `json:"petition"`
+	//Beginning of the request
+	Created time.Time `json:"created"`
+	//Time when response was received
+	Done time.Time `json:"done"`
+}
+
+//newReply returns the Reply for the Petition made, the http.Response gotten and the possible error
+func newReply(resp *http.Response, p *Petition, e error) *Reply {
+	var reply = &Reply{ID: p.ID, Petition: p, TraceID: p.TraceID}
+	if e != nil {
+		reply.Error = e.Error()
+		return reply
+	}
+	reply.StatusCode = resp.StatusCode
+	reply.Proto = resp.Proto
+	reply.Header = resp.Header
+	reply.Trailer = resp.Trailer
+	mylog.Debug("before reading response body", p.ID)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		mylog.Debugf("error reading response body %v %+v", err, p)
+		reply.Error = e.Error()
+	} else {
+		mylog.Debug("after reading response body", p.ID)
+		reply.Body = body
+	}
+	reply.Done = bson.Now()
+	mylog.Debugf("reply done %+v", reply)
+	return reply
+}
